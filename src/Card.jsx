dc7bdc7fc51454b8cbc96ed9d405c5a34c8567c9
Nabil Zafari <zafarinabil@gmail.com>
2023-12-15 16:05:04 +0100

    Filter function

--- src/Card.jsx
+++ src/Card.jsx
@@ -2,10 +2,28 @@ import React from 'react';
 import './Card.css';
 import data from './data/data.json'; // Import the data
 
-const Card = () => {
+const Card = ({ filters }) => {
+	// Check if filters is an array and not empty before applying the filter
+	const filteredJobs = (filters && filters.length > 0
+	  ? data.filter((job) => {
+		  // Check if the job matches all selected filters
+		  return filters.every(filter => {
+			if (filter.startsWith('role-')) {
+			  return job.role === filter.replace('role-', '');
+			} else if (filter.startsWith('level-')) {
+			  return job.level === filter.replace('level-', '');
+			} else if (filter.startsWith('language-')) {
+			  return job.languages.includes(filter.replace('language-', ''));
+			} else if (filter.startsWith('tool-')) {
+			  return job.tools.includes(filter.replace('tool-', ''));
+			}
+			return false;
+		  });
+		})
+	  : data);
 	return (
-		<>
-			{data.map((job) => (
+	  <>
+		{filteredJobs.map((job) => (
 				<div key={job.id} className="card">
 					<img src={job.logo} alt={job.company} />
 
