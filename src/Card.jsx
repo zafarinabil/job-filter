28c6e84e088b3a082b354c445a65e34d63e91146
Nabil Zafari <zafarinabil@gmail.com>
2023-12-15 11:42:03 +0100

    src/Card.jsx updated - getting data from data.json

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,35 +1,42 @@
+import React from 'react';
 import './Card.css';
+import data from './data/data.json'; // Import the data
 
 const Card = () => {
 	return (
 		<>
-			<div className="card">
-				<img src="./images/photosnap.svg" alt="photosnap" />
+			{data.map((job) => (
+				<div key={job.id} className="card">
+					<img src={job.logo} alt={job.company} />
 
-				<div className="card-mini">
-					<div className="top-heading">
-						<p className="company">Photosnap</p>
-						<p className="p-heading">New!</p>
-						<p className="p-heading">Featured</p>
+					<div className="card-mini">
+						<div className="top-heading">
+							<p className="company">{job.company}</p>
+							{job.new && <p className="p-heading">New!</p>}
+							{job.featured && <p className="p-heading">Featured</p>}
+						</div>
+
+						<h2>{job.position}</h2>
+
+						<div className="down-heading">
+							<p className="p-under-heading">{job.postedAt}</p>
+							<p className="p-under-heading">{job.contract}</p>
+							<p className="p-under-heading">{job.location}</p>
+						</div>
 					</div>
 
-					<h2> Senior Frontend Developer</h2>
-
-					<div className="down-heading">
-						<p className="p-under-heading">1d ago</p>
-						<p className="p-under-heading">Full Time</p>
-						<p className="p-under-heading">USA only</p>
+					<div className="right-heading">
+						<p>{job.role}</p>
+						<p>{job.level}</p>
+						{job.languages.map((language) => (
+							<p key={language}>{language}</p>
+						))}
+						{job.tools.map((tool) => (
+							<p key={tool}>{tool}</p>
+						))}
 					</div>
 				</div>
-
-				<div className="right-heading">
-					<p>Frontend</p>
-					<p>Senior</p>
-					<p>HTML</p>
-					<p>CSS</p>
-					<p>JavaScript</p>
-				</div>
-			</div>
+			))}
 		</>
 	);
 };
